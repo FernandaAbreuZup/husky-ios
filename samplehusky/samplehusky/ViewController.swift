8bfc0a6630fe43b47592b8e20d768ecdec10d3ac
Fernanda Abreu <zup04@localiza.com>
2023-05-18 18:48:53 -0300

    feat: initial commit

---
+++ samplehusky/samplehusky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  samplehusky
+//
+//  Created by Fernanda Olimpio Jesus de Abreu on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
